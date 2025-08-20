d2af20adc96202f83d2f6ab7e44da1cb69e7b4e3
msu000000 <msuvi000@gmail.com>
2025-08-20 23:59:52 +0530

    Update SocialShare.tsx

--- src/components/SocialShare.tsx
+++ src/components/SocialShare.tsx
@@ -10,11 +10,12 @@ interface SocialShareProps {
 
 const SocialShare: React.FC<SocialShareProps> = ({ hook, isOpen, onClose }) => {
   const platforms: SharePlatform[] = [
-    {
+   {
       name: 'YouTube',
-       img: 'https://www.youtube.com/s/desktop/84dd6d17/img/favicon_144.png',
-       url: (text: string) => `https://www.youtube.com/results?search_query=${encodeURIComponent(text)}`
-    },
+      icon: '▶️', // or real icon if using React icons
+      color: 'bg-red-600', // match style of other buttons
+      url: (text: string) => `https://www.youtube.com/results?search_query=${encodeURIComponent(text)}`
+    }, 
     {
       name: 'Twitter/X',
       icon: '𝕏',
